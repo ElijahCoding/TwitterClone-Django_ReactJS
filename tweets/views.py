666be94f7d64cab86e381335b9eb8b283099a56a
ElijahCoding <cuiyeqing960904@gmail.com>
2020-05-12 19:23:48 +0300

    bootstrap styles

--- tweets/views.py
+++ tweets/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from .models import Tweet
+import random
 
 def home_view(request, *args, **kwargs):
     return render(request, "pages/home.html", context={}, status=200)
 
 def tweet_list_view(request, *args, **kwargs):
     qs = Tweet.objects.all()
-    tweets_list = [{"id": x.id, "content": x.content} for x in qs]
+    tweets_list = [{"id": x.id, "content": x.content, "likes": random.randint(0, 122)} for x in qs]
     data = {
         "response": tweets_list
     }
