0f6e99b73f9f64299819d9d913cc9ffb82fca1a2
ElijahCoding <cuiyeqing960904@gmail.com>
2020-05-13 17:21:07 +0300

    admin registerg

--- tweets/models.py
+++ tweets/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 import random
+from django.conf import settings
+
+User = settings.AUTH_USER_MODEL
 
 class Tweet(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
-    image = models.FileField(upload_to="images/", blank=True)
+    image = models.FileField(upload_to="images/", blank=True, null=True)
 
     class Meta:
         ordering = ["-id"]
@@ -14,3 +18,6 @@ class Tweet(models.Model):
             "content": self.content,
             "likes": random.randint(0, 200)
         }
+
+    def __str__(self):
+        return self.content
