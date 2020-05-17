d87b42b88c9724b1755e5f2faa7a8f22843da53f
ElijahCoding <cuiyeqing960904@gmail.com>
2020-05-18 00:21:52 +0300

    action ui built

--- tweets/views.py
+++ tweets/views.py
@@ -21,7 +21,7 @@ def home_view(request, *args, **kwargs):
 @authentication_classes([SessionAuthentication])
 @permission_classes([IsAuthenticated])
 def tweet_create_view(request, *args, **kwargs):
-    serializer = TweetSerializer(data=request.POST)
+    serializer = TweetSerializer(data=request.data)
     if serializer.is_valid(raise_exception=True):
         obj = serializer.save(user=request.user)
         return Response(serializer.data, status=201)
@@ -52,7 +52,7 @@ def tweet_delete_view(request, tweet_id, *args, **kwargs):
 @api_view(['POST'])
 @authentication_classes([SessionAuthentication])
 def tweet_action_view(request, tweet_id, *args, **kwargs):
-    serializer = TweetActionSerializer(data=request.POST)
+    serializer = TweetActionSerializer(data=request.data)
     if serializer.is_valid(raise_exception=True):
         data = serializer.validated_data
         tweet_id = data.get("id")
@@ -63,12 +63,14 @@ def tweet_action_view(request, tweet_id, *args, **kwargs):
         obj = qs.first()
         if action == "like":
             obj.likes.add(request.user)
+            serializer = TweetSerializer(obj)
+            return Response(serializer.data, status=200)
         elif action == "unlike":
             obj.likes.remove(request.user)
         elif action == "retweet":
             # todo
             pass
-    return Response({"message": "Tweet removed"}, status=200)
+    return Response("", status=200)
 
 @api_view(['GET'])
 def tweet_list_view(request, *args, **kwargs):
